65b94a7a2e16b33288009075554b4ef97840ea9c
Qodirjon <qodirjonmavlonberdiyev7@gmail.com>
2026-02-28 23:44:35 +0500

    bot

--- app.module.ts
+++ app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from 'src/user/user.module';
-import { QuestionModule } from 'src/question/question.module';
-import { ResultModule } from 'src/result/result.module';
-import { BotModule } from 'src/bot/bot.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath: ".env", isGlobal: true}),
-    MongooseModule.forRoot(process.env.MONGO_URI as string),
-    UserModule,
-    QuestionModule,
-    ResultModule,
-    BotModule,
+    MongooseModule.forRoot(process.env.MONGO_URI as string)
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BotService } from './bot.service';
-import { UserModule } from '../user/user.module';
-import { QuestionModule } from '../question/question.module';
-import { ResultModule } from '../result/result.module';
 
 @Module({
-  imports: [UserModule, QuestionModule, ResultModule],
-  providers: [BotService],
+  imports: [],
+  providers: [],
 })
 export class BotModule {}
